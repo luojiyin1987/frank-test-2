d2a4824e185e50296a055329b24f70c1d4befeae
luojiyin <luojiyin@hotmail.com>
2022-01-14 15:20:09 +0800

    配置分离

--- webpack.config.js
+++ webpack.config.js
@@ -21,23 +21,23 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        })
-    ],
-    externals: {
-        react:{
-            commonjs:'react',
-            commonjs2:'react',
-            amd:'react',
-            root:'React',
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd:'react-dom',
-            root:'ReactDOM',
-        }
-    }
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         template: 'index.html'
+    //     })
+    // ],
+    // externals: {
+    //     react:{
+    //         commonjs:'react',
+    //         commonjs2:'react',
+    //         amd:'react',
+    //         root:'React',
+    //     },
+    //     'react-dom': {
+    //         commonjs: 'react-dom',
+    //         commonjs2: 'react-dom',
+    //         amd:'react-dom',
+    //         root:'ReactDOM',
+    //     }
+    // }
 }
